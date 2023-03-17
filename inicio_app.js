083b1fb92347f1aa4b281afc577aad21412dcf35
HugoA <108324512+Hpp-code@users.noreply.github.com>
2023-03-17 21:19:47 +0100

    Añadido partido San Miguel y episodio 9 de noche de fútbol

--- inicio_app.js
+++ inicio_app.js
@@ -73,7 +73,7 @@ function matchDataDisplay() {
 
 //Countdown
 const timeLeft = document.querySelector("#Inicio .gallery_frame .gallery .frame2 .match-data .countdown-vs .countdown")
-const matchDay = new Date("Mar 12, 2023 11:30:00")
+const matchDay = new Date("Mar 19, 2023 17:00:00")
 const second = 1000
 const minute = second * 60
 const hour = minute * 60
@@ -266,9 +266,10 @@ let ep5 = new Episode ("LUNES NOCHE FÚTBOL DE MADRID #5 | Rosa Torres e Iratxe 
 let ep6 = new Episode ("LUNES NOCHE FÚTBOL DE MADRID #6 | Manuela Alfayate", "/gen_resources/general images/lunes_noche_futbol_blue.jpg", "https://www.youtube.com/live/S7mw2GlnGu8?feature=share&t=5516")
 let ep7 = new Episode ("LUNES NOCHE FÚTBOL DE MADRID #7 | Gabriela de la Villa", "/gen_resources/general images/lunes_noche_futbol_blue.jpg", "https://www.youtube.com/live/70MoHl7iGFs?feature=share&t=5827")
 let ep8 = new Episode ("LUNES NOCHE FÚTBOL DE MADRID #8 | Claudia Domínguez", "/gen_resources/general images/lunes_noche_futbol_blue.jpg", "https://youtu.be/NIC4C8th3SE?t=4430")
+let ep9 = new Episode ("LUNES NOCHE FÚTBOL DE MADRID #9 | Sara Yagüe", "/gen_resources/general images/lunes_noche_futbol_blue.jpg", "https:/https://www.youtube.com/live/mNlmfJrzERs?feature=share&t=6017")
 
 
-arrEps.push(ep8, ep7, ep6, ep5, ep4, ep3, ep2, ep1)
+arrEps.push(ep9, ep8, ep7, ep6, ep5, ep4, ep3, ep2, ep1)
 
 for (let episode of arrEps) {
     epFlex.innerHTML += 
