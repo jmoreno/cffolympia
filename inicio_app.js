4a8c3cae06f2508bf0db6830c0f271f1003f7ac0
HugoA <108324512+Hpp-code@users.noreply.github.com>
2023-03-28 21:10:16 +0200

    Partido Rayo Vallecano añadido

--- inicio_app.js
+++ inicio_app.js
@@ -73,7 +73,7 @@ function matchDataDisplay() {
 
 //Countdown
 const timeLeft = document.querySelector("#Inicio .gallery_frame .gallery .frame2 .match-data .countdown-vs .countdown")
-const matchDay = new Date("Mar 26, 2023 12:00:00")
+const matchDay = new Date("Apr 02, 2023 17:00:00")
 const second = 1000
 const minute = second * 60
 const hour = minute * 60
