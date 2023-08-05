919a756fea12494dddb3a0ab154675b9860cdef0
Hugo <108324512+Hpp-code@users.noreply.github.com>
2023-08-05 20:00:22 +0200

    Add files via upload

---
+++ api/events.mjs
@@ -0,0 +1,16 @@
+//[ ]
+
+let EVENTS = [
+  {
+    name: "WEB 2.0",
+    date: "SEPT 2023",
+    description: "Iniciamos el año estrenando nueva página web.",
+    imgURL: "/public/img/social-index/img1.jpg",
+    largeDescription:
+      "¡Bienvenidas a la nueva web del Olympia! Nuevo diseño y nuevas funcionalidades para estrenar la temporada con ganas.",
+    pageCoverImgURL: "/public/img/social-index/img1.jpg",
+    pageURL: "/pages/eventos.html?evento=web2",
+  },
+];
+
+export default EVENTS;
