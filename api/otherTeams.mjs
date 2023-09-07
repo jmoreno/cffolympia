382cbe29de9648d29fde411686467b3642de6861
H <hugoprietopetrossi@gmail.com>
2023-09-07 16:00:35 +0200

    Cleaning branch from error

--- api/otherTeams.mjs
+++
@@ -1,8 +0,0 @@
-//RIGHT NOW IS ONLY A DOC TO SAVE THE LINK TO EACH TEAMS SHIELD
-//[ ]
-
-let otherTeams = {
-  atleti: {
-    imgURL: "public/img/escudos/atleti.png",
-  },
-};
--- readme.md
+++
@@ -1,4 +0,0 @@
-## WEB CDB FF Olympia
-### Links desarrollo web Olympia
-- [Web principal](https://www.cffolympia.es)
-- [Web de desarrollo](https://dev.cffolympia.es)
